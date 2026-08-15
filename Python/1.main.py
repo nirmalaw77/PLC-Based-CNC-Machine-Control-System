7fabc08dd4c73d871654f1a5663168d1d1558f8b
Nirmala Warushavithana <nirmalawaruse@gmail.com>
2026-08-15 23:57:51 +0530

    Delete Python directory

--- Python/1.main.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from gui_app import start_gui   ##Called Start_gui Function in gui_app.py file from main.py
-
-if __name__ == "__main__": ## Check availability of main.py file
-    start_gui()
--- Python/page_flags.py
+++
@@ -1,9 +0,0 @@
-# page_flags.py
-
-monitor_active = False
-manual_active = False
-cnc_active = False
-cnc2_active = False
-gcode_active = False
-settings_active = False
-homing_active = False
